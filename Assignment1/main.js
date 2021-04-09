2bdbc47bec7c728ec6d7863a76e59f34e8829832
ujwalsingla <ujwalsingla2577@gmail.com>
2021-04-09 16:46:25 +0530

    Assignment 1 Update

--- Assignment1/main.js
+++ Assignment1/main.js
@@ -16,7 +16,7 @@ function onSubmit(e) {
         setTimeout(()  => msg.remove(), 3000);
     } else {
         const li = document.createElement('li');
-        li.appendChild(document.createTextNode('${nameInput.value} : ${emailInput.value}'));
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
 
         userList.appendChild(li);
 
